7a71e700ee28c439f65c049334aed85ed899a8ea
Blaine Schanfeldt <git@blaines.me>
2016-02-26 10:49:24 -0800

    adding resync option

--- helpers/aws-entrypoint.sh
+++ helpers/aws-entrypoint.sh
@@ -6,4 +6,4 @@ if [ -z "$LOCALIPV4" ]; then
 	export LOCALIPV4=$(curl --retry 5 --connect-timeout 3 -s 169.254.169.254/latest/meta-data/local-ipv4)
 fi
 
-/bin/registrator "-ip=$LOCALIPV4" "consul://$LOCALIPV4:8500"
+/bin/registrator -ip=$LOCALIPV4 -resync 60 consul://$LOCALIPV4:8500
